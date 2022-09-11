0ddd9270b1c1a9a91e45f73206e49537055734b6
Olga Sokolinskaya <olga_genplan@hotmail.com>
2022-09-11 20:03:47 +0300

    Initial Commit

---
+++ src/AcademyHw/Hw1/Calculator.java
@@ -0,0 +1,15 @@
+package AcademyHw.Hw1;
+
+//import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        //double num1 =
+    }
+
+    //Scanner scanner = new Scanner(System.in);
+
+
+
+
+}
